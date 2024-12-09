89ff37f0fd5cb49b40b0bc61d8a64eedbfe68de9
liaokun <liaokun@egova.com.cn>
2024-12-09 20:04:39 +0800

    fix: 解决重复收集导致自定义指标报错的问题

--- collector/custom_collector.go
+++ collector/custom_collector.go
@@ -118,14 +118,12 @@ func (cm *CustomMetrics) Collect(ch chan<- prometheus.Metric) {
 					logger.Logger.Debugf("metric %s value %v labelValues %v", field, conver_float, labelValues)
 				}
 			}
-
 		}
+	}
 
-		// 手动调用 Prometheus 的 Collect 方法来注册所有已更新的 metrics
-		for _, collector := range cm.metrics {
-			collector.Collect(ch)
-		}
-
+	// 手动调用 Prometheus 的 Collect 方法来注册所有已更新的 metrics
+	for _, collector := range cm.metrics {
+		collector.Collect(ch)
 	}
 }
 
